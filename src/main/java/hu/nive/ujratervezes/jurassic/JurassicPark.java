556a2c5551083002524775fb33953d372c9738a3
henimia <henimia@gmail.com>
2021-04-29 11:29:29 +0200

    Add dbjurassicpark

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -1,5 +1,10 @@
 package hu.nive.ujratervezes.jurassic;
 
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JurassicPark {
 
     private String dbUrl;
@@ -12,4 +17,53 @@ public class JurassicPark {
         this.dbPassword = dbPassword;
     }
 
+    public List<String> checkOverpopulation() {
+        List<String> overpopulation = new ArrayList<>();
+        String sql = "SELECT * FROM dinosaur WHERE expected < actual ORDER BY breed";
+        try (Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                overpopulation.add(resultSet.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return overpopulation;
+    }
 }
+
+
+/*package hu.nive.ujratervezes.jurassic;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JurassicPark {
+
+    private String dbUrl;
+    private String dbUser;
+    private String dbPassword;
+
+    public JurassicPark(String dbUrl, String dbUser, String dbPassword) {
+        this.dbUrl = dbUrl;
+        this.dbUser = dbUser;
+        this.dbPassword = dbPassword;
+    }
+
+    public List<String> checkOverpopulation() {
+        List<String> overpopulationDinosaurs = new ArrayList<>();
+        String sql = "SELECT * FROM dinosaur WHERE expected < actual ORDER BY breed";
+        try (Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                overpopulationDinosaurs.add(resultSet.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return overpopulationDinosaurs;
+    }
+}*/
