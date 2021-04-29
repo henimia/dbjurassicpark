c63e5cb5b67a03d9faf3041dcd2a24f0ab3e9472
henimia <henimia@gmail.com>
2021-04-29 11:50:34 +0200

    Add javított dbjurassicpark

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -34,36 +34,3 @@ public class JurassicPark {
 }
 
 
-/*package hu.nive.ujratervezes.jurassic;
-
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-
-public class JurassicPark {
-
-    private String dbUrl;
-    private String dbUser;
-    private String dbPassword;
-
-    public JurassicPark(String dbUrl, String dbUser, String dbPassword) {
-        this.dbUrl = dbUrl;
-        this.dbUser = dbUser;
-        this.dbPassword = dbPassword;
-    }
-
-    public List<String> checkOverpopulation() {
-        List<String> overpopulationDinosaurs = new ArrayList<>();
-        String sql = "SELECT * FROM dinosaur WHERE expected < actual ORDER BY breed";
-        try (Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
-             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                overpopulationDinosaurs.add(resultSet.getString(1));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return overpopulationDinosaurs;
-    }
-}*/
